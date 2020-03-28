26baf84b6ddc42d2318a1ad78bab122d1f86f6df
Yaser Eftekhari <yeftekhari@gmail.com>
2020-03-28 01:12:15 +0100

    Some tests, Automatic Compacting

--- src/main/java/me/yeuser/graph/core/VerticesMap.kt
+++ src/main/java/me/yeuser/graph/core/VerticesMap.kt
@@ -7,38 +7,52 @@ import java.util.concurrent.locks.ReentrantReadWriteLock
 import kotlin.math.max
 import kotlin.math.min
 
-class VerticesMap {
-  private val lock: ReadWriteLock =
-    ReentrantReadWriteLock()
+class VerticesMap(
+  private val bufferOpsLimit: Int = 8 * 1024,
+  private val nodeEdgeExpansionMax: Int = 1024 * 1024,
+  private val nodeEdgeExpansionMin: Int = 1024
+) {
+
+  private val lock: ReadWriteLock = ReentrantReadWriteLock()
   private val vertices: Int2ObjectOpenHashMap<IntRBTreeSet> =
     Int2ObjectOpenHashMap(32 * 1024, 1f)
   private var verticesCompact: Array<BigIntSet?> = emptyArray()
-  private var cw = 0
+  private var cOps = 0
 
   fun add(fromIdx: Int, toIdx: Int) {
     lock.writeLock().lock()
     vertices.computeIfAbsent(fromIdx) { IntRBTreeSet() }.add(toIdx)
-    cw++
-    if (cw > 8 * 1024) {
-      vertices.forEach { (key, value) ->
-        if (verticesCompact.size <= key) {
-          val delta = min(
-            1024 * 1024,
-            max(1024, key + 1 - verticesCompact.size) * 2
-          )
-          verticesCompact = verticesCompact.copyOf(verticesCompact.size + delta)
-        }
-        var v = verticesCompact[key]
-        if (v == null) {
-          v = BigIntSet()
-          verticesCompact[key] = v
-        }
-        v.addAll(value.toIntArray())
-      }
-      vertices.clear()
-      cw = 0
+    lock.writeLock().unlock()
+    checkShrink()
+  }
+
+  private fun checkShrink() {
+    lock.writeLock().lock()
+    cOps++
+    if (cOps > bufferOpsLimit) {
+      compact()
+      cOps = 0
     }
     lock.writeLock().unlock()
+  }
+
+  private fun compact() {
+    vertices.forEach { (key, value) ->
+      if (verticesCompact.size <= key) {
+        val delta = min(
+          nodeEdgeExpansionMax,
+          max(nodeEdgeExpansionMin, key + 1 - verticesCompact.size) * 2
+        )
+        verticesCompact = verticesCompact.copyOf(verticesCompact.size + delta)
+      }
+      var v = verticesCompact[key]
+      if (v == null) {
+        v = BigIntSet()
+        verticesCompact[key] = v
+      }
+      v.addAll(value.toIntArray())
+    }
+    vertices.clear()
   }
 
   fun get(fromIdx: Int): Iterable<Int>? {
@@ -48,6 +62,7 @@ class VerticesMap {
       verticesCompact.takeIf { it.size > fromIdx }?.get(fromIdx)?.asSequence()?.asIterable()
     ).flatten()
     lock.readLock().unlock()
+    checkShrink()
     return rbTreeSet.takeUnless { it.isEmpty() }
   }
 }
---
+++ src/test/java/me/yeuser/graph/core/test/VerticesMapTester.kt
@@ -0,0 +1,29 @@
+package me.yeuser.graph.core.test
+
+import me.yeuser.graph.core.VerticesMap
+import org.junit.Test
+import java.util.*
+
+class VerticesMapTester {
+
+  @Test
+  fun testFunctionality() {
+    val nodesCount = 100
+    val random = Random()
+    val vertices = VerticesMap(bufferOpsLimit = 200)
+    val nodeEdges = (0 until nodesCount).associateWith { i ->
+      (0 until nodesCount).filter { j ->
+        i != j && random.nextBoolean()
+      }
+    }
+
+    nodeEdges.forEach { (from, toN) -> toN.forEach { vertices.add(from, it) } }
+
+    nodeEdges.forEach { (from, tos) ->
+      val expected = tos.toSet()
+      val actual = vertices.get(from)!!.toSet()
+      assert(actual == expected) { "$actual != $expected" }
+    }
+  }
+
+}
