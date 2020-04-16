ef868c9316d73c9315bcd5d0dcec454cd3ca11cd
Yaser Eftekhari <yeftekhari@gmail.com>
2020-04-17 00:22:30 +0200

    Reformatting

--- src/test/kotlin/me/yeuser/graph/impl/primitivearray/BigIntSetTester.kt
+++ src/test/kotlin/me/yeuser/graph/impl/primitivearray/BigIntSetTester.kt
@@ -1,9 +1,9 @@
 package me.yeuser.graph.impl.primitivearray
 
-import org.junit.jupiter.api.Test
 import java.util.Random
 import kotlin.streams.toList
 import kotlin.test.assertEquals
+import org.junit.jupiter.api.Test
 
 class BigIntSetTester {
     private val random = Random()
@@ -27,14 +27,17 @@ class BigIntSetTester {
         (1..100).map {
             random.ints(50L + random.nextInt(700), 5, 30_000).distinct().toList() to
                 random.ints(50L + random.nextInt(700), 5, 30_000).distinct().toList()
-        }.withIndex().forEach { (index, testSet ) ->
+        }.withIndex().forEach { (index, testSet) ->
             val (insertSet, deleteSet) = testSet
             allInts.addAll(insertSet)
             allInts.removeAll(deleteSet)
-            print("Test#${index + 1}=> length inserts:${insertSet.size}, length deletes:${deleteSet.size}, final size: ${allInts.size}")
+            print("Test#${index + 1}=> ")
+            print("length inserts:${insertSet.size}, ")
+            print("length deletes:${deleteSet.size}, ")
+            print("final size: ${allInts.size}")
 
             bigIntSet.addAll(insertSet)
-            insertSet.forEach { testNumber -> assert(bigIntSet.has(testNumber)){
+            insertSet.forEach { testNumber -> assert(bigIntSet.has(testNumber)) {
                 "$bigIntSet, $testNumber"
             } }
             bigIntSet.removeAll(deleteSet)
