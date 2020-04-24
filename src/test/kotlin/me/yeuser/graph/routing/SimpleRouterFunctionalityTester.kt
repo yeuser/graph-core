c19468703544558a04a7a87ca49ec7c0f4576a81
Yaser Eftekhari <yeftekhari@gmail.com>
2020-04-24 10:37:18 +0200

    Added routing test with high complexity

--- src/test/kotlin/me/yeuser/graph/routing/SimpleRouterFunctionalityTester.kt
+++ src/test/kotlin/me/yeuser/graph/routing/SimpleRouterFunctionalityTester.kt
@@ -4,33 +4,102 @@ import io.kotest.matchers.doubles.plusOrMinus
 import io.kotest.matchers.shouldBe
 import me.yeuser.graph.blocks.fastmap.FastMapEdgeIndexer
 import org.junit.jupiter.api.Test
+import java.util.Queue
+import java.util.Random
+import java.util.concurrent.ConcurrentLinkedQueue
 
 class SimpleRouterFunctionalityTester {
 
     @Test
-    fun `test simple routing`() {
-        val type1 = "t1"
-        val edges = FastMapEdgeIndexer(100, 10_000, type1)
-        edges.add(1, 2, type1, .45, false)
-        edges.add(2, 3, type1, .35, false)
+    fun `test simple routing FastMapEdgeIndexer`() {
+        val t = "t"
+        val edges = FastMapEdgeIndexer(100, 10_000, t)
+        edges.add(1, 2, t, .45, false)
+        edges.add(2, 3, t, .35, false)
 
         val router = SimpleGraphRouter(edges)
         router.route(1, 2) shouldBe listOf(1 to .0, 2 to .45)
         router.route(2, 3) shouldBe listOf(2 to .0, 3 to .35)
         router.route(1, 3) shouldBe listOf(1 to .0, 2 to .45, 3 to .8)
 
-        edges.add(1, 3, type1, .91, false)
+        edges.add(1, 3, t, .91, false)
         router.route(1, 3) shouldBe listOf(1 to .0, 2 to .45, 3 to .8)
 
-        edges.add(1, 3, type1, .79, false)
+        edges.add(1, 3, t, .79, false)
         router.route(1, 3) shouldBe listOf(1 to .0, 3 to .79)
     }
 
-    fun List<Pair<Int, Double>>.shouldBe(expected: List<Pair<Int, Double>>) {
+    @Test
+    fun `test complex mesh routing FastMapEdgeIndexer`() {
+        val MESH_COUNT = 4
+        val MESH_NODE_COUNT = 100
+
+        val t = "t"
+        val edges = FastMapEdgeIndexer(100, 10_000, t)
+
+        // initialize random data
+        val random = Random()
+
+        val connections = Array(MESH_COUNT * MESH_NODE_COUNT) { mutableMapOf<Int, Double>() }
+        repeat(MESH_COUNT) { k -> // #islands of meshed graphs
+            repeat(MESH_NODE_COUNT) { i -> // from #nodes
+                repeat(MESH_NODE_COUNT) { j -> // to #nodes
+                    // randomly (50-50 chance) connect or disconnect the nodes with random weight
+                    if (i != j && random.nextBoolean())
+                        connections[k * MESH_NODE_COUNT + i][k * MESH_NODE_COUNT + j] = random.nextDouble()
+                }
+            }
+        }
+        repeat(MESH_COUNT) { k1 ->// from source mesh
+            repeat(MESH_COUNT) { k2 ->// to destination mesh
+                if (k1 != k2) repeat(2) { // connect two nodes from source mesh to two nodes at destination mesh
+                    val i = random.nextInt(MESH_NODE_COUNT)
+                    val j = random.nextInt(MESH_NODE_COUNT)
+                    connections[k1 * MESH_NODE_COUNT + i][k2 * MESH_NODE_COUNT + j] = random.nextDouble()
+                }
+
+            }
+        }
+
+        // insert generated connection data into edge-indexer
+        connections.forEachIndexed { i, cons ->
+            cons.forEach { (j, w) ->
+                edges.add(i, j, t, w, false)
+            }
+        }
+
+        // randomly do find routes
+        repeat(200) {
+            val bestPaths = mutableMapOf<Int, Pair<Double, IntArray>>()
+            val queue: Queue<Pair<Int, Int>> = ConcurrentLinkedQueue()
+            val i = random.nextInt(MESH_NODE_COUNT * MESH_COUNT)
+            bestPaths[i] = 0.0 to intArrayOf(i)
+            connections[i].forEach { (j, w) ->
+                queue.add(i to j)
+                bestPaths[j] = w to intArrayOf(i, j)
+            }
+            while (queue.isNotEmpty()) {
+                val (prv, cur) = queue.remove()
+                connections[cur].forEach { (nxt, dw) ->
+                    val w = bestPaths[cur]!!.first + dw
+                    if (bestPaths[nxt]?.first ?: Double.MAX_VALUE > w) {
+                        queue.add(cur to nxt)
+                        bestPaths[nxt] = w to bestPaths[cur]!!.second + nxt
+                    }
+                }
+            }
+            val j = random.nextInt(MESH_NODE_COUNT * MESH_COUNT)
+
+            val router = SimpleGraphRouter(edges)
+            router.route(i, j) shouldBe bestPaths[j]!!.second.map { it to bestPaths[it]!!.first }
+        }
+    }
+
+    private infix fun List<Pair<Int, Double>>.shouldBe(expected: List<Pair<Int, Double>>) {
         size shouldBe expected.size
         forEachIndexed { i, (n, w) ->
             n shouldBe expected[i].first
-            w shouldBe (expected[i].second plusOrMinus 1E-4)
+            w shouldBe (expected[i].second plusOrMinus 1E-3)
         }
     }
 }
